64c8b2ff3b6dfe28315280625715599eb48afbac
Lucas Vieira <ls6182315@gmail.com>
2023-03-23 09:02:32 -0300

    fix errors

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,12 +1,12 @@
 import ICar from '../Interfaces/ICar';
 
 class Car {
-  private id?: string;
-  private model: string;
-  private year: number;
-  private color: string;
-  private status?: boolean;
-  private buyValue: number;
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
   private doorsQty: number;
   private seatsQty: number;
 
@@ -21,6 +21,70 @@ class Car {
     this.buyValue = car.buyValue;
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setId(id:string) {
+    this.id = id;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setModel(model:string) {
+    this.model = model;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setYear(year:number) {
+    this.year = year;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setColor(color:string) {
+    this.color = color;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setStatus(status:boolean) {
+    this.status = status;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setBuyValue(buyValue:number) {
+    this.buyValue = buyValue;
+  }
+
+  public getDoorsQty() {
+    return this.doorsQty;
+  }
+
+  public setDoorsQty(doorsQty:number) {
+    this.doorsQty = doorsQty;
+  }
+
+  public getSeatsQty() {
+    return this.seatsQty;
+  }
+
+  public setSeatsQty(seatsQty:number) {
+    this.seatsQty = seatsQty;
   }
 }
 
