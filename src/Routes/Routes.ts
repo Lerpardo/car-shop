4e32f8b42eeec2c97c1d14cba19829b8403d3a92
Lucas Vieira <ls6182315@gmail.com>
2023-03-23 19:13:27 -0300

    car routes

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -8,4 +8,16 @@ const carRouter = Router();
 
 carRouter.post('/cars', jsonParser, (req, res, next) => new CarController(req, res, next).create());
 
+carRouter.get('/cars/:id', (req, res, next) => new CarController(req, res, next).findById());
+
+carRouter.get('/cars', (req, res, next) => new CarController(req, res, next).findAll());
+
+carRouter.put(
+  '/cars/:id',
+  jsonParser,
+  (req, res, next) => new CarController(req, res, next).update(),
+);
+
+carRouter.delete('/cars/:id', (req, res, next) => new CarController(req, res, next).delete());
+
 export default carRouter;
