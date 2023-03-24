04bafbba498beabd97dd0f1109271c7510248282
Lucas Vieira <ls6182315@gmail.com>
2023-03-24 00:25:35 -0300

    add moto routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import routes from './Routes/Routes';
+import carRoutes from './Routes/car.routes';
+import motoRoutes from './Routes/moto.routes';
 import ErrorHandler from './Middlewares/ErrorHandler';
 
 const app = express();
-app.use(routes);
+app.use(carRoutes);
+app.use(motoRoutes);
 app.use(ErrorHandler.handle);
 
 export default app;
