932cb6e719b585aa0e8235c9ac094bee2cbcc8b0
Lucas Vieira <ls6182315@gmail.com>
2023-03-24 20:39:01 +0000

    fix lint

--- tests/unit/Services/mocks/moto.mock.ts
+++ tests/unit/Services/mocks/moto.mock.ts
@@ -1,27 +1,27 @@
-import IMotorcycle from "../../../../src/Interfaces/IMotorcycle";
-import Motorcycle from "../../../../src/Domains/Motorcycle";
+import IMotorcycle from '../../../../src/Interfaces/IMotorcycle';
+import Motorcycle from '../../../../src/Domains/Motorcycle';
 
 const newMotorcycleInput: IMotorcycle = {
-    "model": "Honda Cb 600f Hornet",
-    "year": 2005,
-    "color": "Yellow",
-    "status": true,
-    "buyValue": 30.000,
-    "category": "Street",
-    "engineCapacity": 600
-  }
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
 
 const newMotorcycleOutput: Motorcycle = new Motorcycle(
-    {
-        "id": "6348513f34c397abcad040b2",
-        "model": "Honda Cb 600f Hornet",
-        "year": 2005,
-        "color": "Yellow",
-        "status": true,
-        "buyValue": 30.000,
-        "category": "Street",
-        "engineCapacity": 600
-      }
-)
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+);
 
 export { newMotorcycleInput, newMotorcycleOutput };
