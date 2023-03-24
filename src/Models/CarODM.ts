4f82240dac403cb68d9b69bfe260bc79f0d69a40
Lucas Vieira <ls6182315@gmail.com>
2023-03-23 22:49:24 -0300

    extends carOdm to abstractOdm

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -1,12 +1,10 @@
-import { Model, Schema, model, models } from 'mongoose';
+import { Schema } from 'mongoose';
 import ICar from '../Interfaces/ICar';
+import AbstractODM from './AbstractODM';
 
-class CarODM {
-  private schema: Schema;
-  private model: Model<ICar>;
-
+class CarODM extends AbstractODM<ICar> {
   constructor() {
-    this.schema = new Schema<ICar>({
+    const schema = new Schema<ICar>({
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
@@ -15,27 +13,8 @@ class CarODM {
       doorsQty: { type: Number, required: true },
       seatsQty: { type: Number, required: true },
     });
-    this.model = models.Car || model('cars', this.schema);
-  }
 
-  public async create(car: ICar): Promise<ICar> {
-    return this.model.create({ ...car });
-  }
-
-  public async findById(id: string): Promise<ICar | null> {
-    return this.model.findById(id);
-  }
-
-  public async findAll(): Promise<ICar[]> {
-    return this.model.find();
-  }
-
-  public async update(id: string, car: ICar): Promise<ICar | null> {
-    return this.model.findByIdAndUpdate(id, car, { new: true });
-  }
-
-  public async delete(id: string): Promise<ICar | null> {
-    return this.model.findByIdAndDelete(id);
+    super(schema, 'cars');
   }
 }
 
